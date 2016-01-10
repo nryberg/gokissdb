26dc1e26b20587e99306529169c8e133b970d7b0
Nick Ryberg <nryberg@gmail.com>
2016-01-10 17:35:46 -0600

    Pulls in first half of header file in GoLang

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # gokissdb
+
+Go KissDB is a golang riff on Adam Ierymenko's ultra simple, open licensed Key
+Value store [KissDB](https://github.com/zerotier/kissdb).
+
+As much as learning how a basic K/V store works, this is an opportunity to get
+better at Go and develop a platform for data storage for the [Analytic Kata](https://github.com/nryberg/analytic-kata.git) learning series.  
+
+Goals include reasonable compatibility with Adam's KissDB, speed and simplicity.
--- testbed.go
+++ testbed.go
@@ -4,6 +4,32 @@ import (
 	"log"
 )
 
+// For now these are near perfect but potentially
+// uneccesary copies of the constants in the
+// source C files.  Delete if you actually don't
+// need them.
+
+const KISSDB_VERSION int = 2
+const KISSDB_ERROR_IO int = -1
+const KISSDB_ERROR_MALLOC int = -2
+const KISSDB_ERROR_INVALID_PARAMETERS int = -3
+const KISSDB_ERROR_CORRUPT_DBFILE int = -4
+const KISSDB_OPEN_MODE_RDONLY int = 1
+const KISSDB_OPEN_MODE_RDWR int = 2
+const KISSDB_OPEN_MODE_RWCREAT int = 3
+const KISSDB_OPEN_MODE_RWREPLACE int = 4
+
+type KISSDB struct {
+	hash_table_size       int32
+	key_size              int32
+	value_size            int32
+	hash_table_size_bytes int32
+	num_hash_tables       int32
+	// Need to add pointers to hash_tables
+	// and the working file
+}
+
 func main() {
 	log.Println("Starting Kiss DB")
+	log.Println("Version: ", KISSDB_VERSION)
 }
